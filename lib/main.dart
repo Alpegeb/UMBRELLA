d4f67bbeb0cf8f65bce5258a25b7d54746185ef0
aysinnuripek <aysin.ipek@sabanciuniv.edu>
2025-12-22 23:47:02 +0300

    Fix import paths in main.dart
    Updated import paths for app_router and data_provider.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
-import 'router/app_router.dart';
+import 'package:umbrella/router/app_router.dart'; // düzeltildi
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:provider/provider.dart';
+import 'package:umbrella/providers/data_provider.dart'; // provider importu
 
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const UmbrellaApp());
+}
+
+class UmbrellaApp extends StatelessWidget {
+  const UmbrellaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => DataProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: AppRouter.generateRoute, // AppRouter burada kullanılacak
+        initialRoute: '/',
+      ),
+    );
+  }
 }
