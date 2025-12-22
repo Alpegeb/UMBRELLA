6ee0173f49bca2a8d3cdad02543688291424c9bb
aysinnuripek <aysin.ipek@sabanciuniv.edu>
2025-12-22 18:44:30 +0300

    Integrate provider and setup MultiProvider in main.dart
    Added provider dependencies and initialized MultiProvider for state management.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
-import 'router/app_router.dart';
+import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 import 'firebase_options.dart';
+import 'router/app_router.dart';
+import 'providers/auth_provider.dart';
+import 'providers/data_provider.dart';
 
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const UmbrellaApp());
+}
+
+class UmbrellaApp extends StatelessWidget {
+  const UmbrellaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => DataProvider()),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+      ),
+    );
+  }
 }
