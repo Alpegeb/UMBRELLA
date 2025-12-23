cc57327377af420d2ea2777025f5ebe211baa3df
aysinnuripek <aysin.ipek@sabanciuniv.edu>
2025-12-23 15:21:34 +0300

    Implement ThemePrefStore for theme preference management
    Added ThemePrefStore class for managing theme preferences using SharedPreferences.

--- lib/core/theme_pref.dart
+++ lib/core/theme_pref.dart
@@ -1,1 +1,22 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 enum ThemePref { light, system, dark }
+
+class ThemePrefStore {
+  static const _k = 'theme_pref';
+
+  Future<ThemePref> load() async {
+    final sp = await SharedPreferences.getInstance();
+    final v = sp.getString(_k) ?? ThemePref.system.name;
+
+    return ThemePref.values.firstWhere(
+      (e) => e.name == v,
+      orElse: () => ThemePref.system,
+    );
+  }
+
+  Future<void> save(ThemePref pref) async {
+    final sp = await SharedPreferences.getInstance();
+    await sp.setString(_k, pref.name);
+  }
+}
