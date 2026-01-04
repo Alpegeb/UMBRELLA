451b56bdcfdab1c1607376c7909d10a9b8c0ea03
nisaakan <nisa.kan@sabanciuniv.edu>
2026-01-04 14:10:47 +0300

    Update app_launch_test.dart

--- integration_test/app_launch_test.dart
+++ integration_test/app_launch_test.dart
@@ -1,19 +1,15 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'package:umbrella/main_test.dart' as app;
+import 'package:umbrella/main_test.dart' as test_app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('App launches successfully', (tester) async {
-    await app.main();
+  testWidgets('App launches (integration smoke test)', (tester) async {
+    test_app.main();
+    await tester.pumpAndSettle(const Duration(seconds: 2));
 
-    // Let first frames + async inits settle
-    await tester.pumpAndSettle(const Duration(seconds: 5));
-
-    // Simple assertion: app builds a MaterialApp somewhere
-    expect(find.byType(MaterialApp), findsOneWidget);
+    expect(find.byKey(const Key('integrationReady')), findsOneWidget);
   });
 }
