07bfb0743a4af977ce5a7f2f19944426c7f2e3f2
nisaakan <nisa.kan@sabanciuniv.edu>
2026-01-04 13:44:39 +0300

    Update settings_state_unit_test.dart

--- test/settings_state_unit_test.dart
+++ test/settings_state_unit_test.dart
@@ -15,8 +15,8 @@ void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   test('SettingsState persists preferences across instances', () async {
+    // Arrange
     SharedPreferences.setMockInitialValues({});
-
     final s1 = SettingsState();
     await _waitLoaded(s1);
 
@@ -24,6 +24,7 @@ void main() {
     final oldWind = s1.windInKph;
     final oldUmb = s1.showUmbrellaIndex;
 
+    // Act
     await s1.setUseCelsius(!oldUseC);
     await s1.setWindInKph(!oldWind);
     await s1.setShowUmbrellaIndex(!oldUmb);
@@ -31,6 +32,7 @@ void main() {
     final s2 = SettingsState();
     await _waitLoaded(s2);
 
+    // Assert
     expect(s2.useCelsius, equals(!oldUseC));
     expect(s2.windInKph, equals(!oldWind));
     expect(s2.showUmbrellaIndex, equals(!oldUmb));
