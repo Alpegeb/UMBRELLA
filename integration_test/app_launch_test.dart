0ba6ebb622bb44d8b31f4978c0813f1ac0218d91
nisaakan <nisa.kan@sabanciuniv.edu>
2026-01-04 14:14:31 +0300

    Update app_launch_test.dart

--- integration_test/app_launch_test.dart
+++ integration_test/app_launch_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
@@ -7,7 +8,8 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('App launches (integration smoke test)', (tester) async {
-    test_app.main();
+    test_app.main(); // <-- await yok
+
     await tester.pumpAndSettle(const Duration(seconds: 2));
 
     expect(find.byKey(const Key('integrationReady')), findsOneWidget);
