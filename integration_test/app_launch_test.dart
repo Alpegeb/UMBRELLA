6c1cad125a8bb7d4f7f5226d39b7b2cc933e5d60
nisaakan <nisa.kan@sabanciuniv.edu>
2026-01-04 14:00:34 +0300

    Update app_launch_test.dart

--- integration_test/app_launch_test.dart
+++ integration_test/app_launch_test.dart
@@ -2,20 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-// test entrypoint (no Firebase / Workmanager / notifications)
-import 'package:umbrella/main_test.dart' as test_app;
+import '../lib/main_test.dart' as test_app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('App launches (integration smoke test)', (tester) async {
-    // Arrange + Act
     test_app.main();
 
-    // Let the app build and settle
+    // Let initial async work settle (routing, providers, etc.)
     await tester.pumpAndSettle(const Duration(seconds: 5));
 
-    // Assert
+    // If the app launches, we should have a MaterialApp somewhere in the tree.
     expect(find.byType(MaterialApp), findsWidgets);
   });
 }
